f9ae2bf83a75651f0b1847ad0cd7d36b21dfd363
NEXRO <105696039+nex-ro@users.noreply.github.com>
2025-05-29 01:03:13 +0700

    palindrome

---
+++ 2131.py
@@ -0,0 +1,25 @@
+# You are given an array of strings words. Each element of words consists of two lowercase English letters.
+
+# Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+
+# Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+
+# A palindrome is a string that reads the same forward and backward.
+# Example 2:
+
+# Input: words = ["ab","ty","yt","lc","cl","ab"]
+# Output: 8
+# Explanation: One longest palindrome is "ty" + "lc" + "cl" + "yt" = "tylcclyt", of length 8.
+# Note that "lcyttycl" is another longest palindrome that can be created.
+# Example 3:
+
+# Input: words = ["cc","ll","xx"]
+# Output: 2
+# Explanation: One longest palindrome is "cc", of length 2.
+# Note that "ll" is another longest palindrome that can be created, and so is "xx".
+
+words=input("word :").split(" ")
+
+
+
+
